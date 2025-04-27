c125817209872116e7188c8cc0157946e236667c
William He <contact@williamhe.dev>
2025-04-26 17:53:35 -0700

    feat: implement basic BST insertion

--- A2/src/UnorderedSet.cpp
+++ A2/src/UnorderedSet.cpp
@@ -1,7 +1,47 @@
 #include "../include/UnorderedSet.h"
 
-template<typename keyname>
-UnorderedSet<keyname>::UnorderedSet() {}
+template<typename Key>
+UnorderedSet<Key>::UnorderedSet() : setSize(0) {
+}
 
-template<typename keyname>
-UnorderedSet<keyname>::~UnorderedSet() {}
+template<typename Key>
+UnorderedSet<Key>::~UnorderedSet() {
+    // TODO: Implement deconstructor. Need to reassign nodes
+}
+
+template<typename Key>
+bool UnorderedSet<Key>::insert(const Key &key) {
+    // Check if the key already exists
+    if (search(key)) return false;
+
+    Node<Key> *newNode(key);
+    Node<Key> currentNode(root);
+
+    if (currentNode == nullptr) {
+        newNode->color = Color::BLACK;
+        root = newNode;
+    } else {
+        while (currentNode != nullptr) {
+            if (key == currentNode->key) return false;
+
+            Node<Key> parent = currentNode;
+            currentNode = key < currentNode.key ? currentNode.left : currentNode.right;
+
+            if (currentNode == nullptr) {
+                if (parent.color == Color::RED) newNode->color = Color::BLACK;
+
+                if (key < parent->key) {
+                    parent->left = newNode;
+                } else {
+                    parent->right = newNode;
+                }
+            }
+        }
+
+        // Handle violations
+    }
+
+    setSize += 1;
+
+    return true;
+}
