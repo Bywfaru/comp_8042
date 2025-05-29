d30c3006d248f159d7a7e1b928da0f3963f900cb
William He <contact@williamhe.dev>
2025-05-28 22:14:20 -0700

    feat: implement MergeSort

--- A3/src/MergeSort.cpp
+++ A3/src/MergeSort.cpp
@@ -10,15 +10,40 @@ void MergeSort<T>::sort(std::vector<T> &arr) {
 
     const int middleIndex = arr.size() / 2;
     std::vector<T> left(arr.begin(), arr.begin() + middleIndex);
-    std::vector<T> right(arr.begin() + middleIndex + 1, arr.end());
+    std::vector<T> right(arr.begin() + middleIndex, arr.end());
+
+    sort(left);
+    sort(right);
+
+    merge(arr, left, right);
 }
 
 /**
- *
- * @param arr
- * @param left
- * @param right
+ * Merges the two arrays into a sorted array.
+ * @param arr The array to insert the sorted values into
+ * @param left The left array to merge
+ * @param right The right array to merge
  */
 template<typename T>
 void MergeSort<T>::merge(std::vector<T> &arr, const std::vector<T> &left, const std::vector<T> &right) {
+    int leftIndex = 0;
+    int rightIndex = 0;
+
+    while (leftIndex < left.size() && rightIndex < right.size()) {
+        if (compare(left[leftIndex], right[rightIndex])) {
+            arr[leftIndex + rightIndex] = left[leftIndex];
+            leftIndex += 1;
+        } else {
+            arr[leftIndex + rightIndex] = right[rightIndex];
+            rightIndex += 1;
+        }
+    }
+
+    for (int i = leftIndex; i < left.size(); i++) {
+        arr[i + rightIndex] = left[i];
+    }
+
+    for (int i = rightIndex; i < right.size(); i++) {
+        arr[i + leftIndex] = right[i];
+    }
 }
