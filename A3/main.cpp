bdcb929e401546f01c08a9d5811511a98964ff95
William He <contact@williamhe.dev>
2025-06-03 00:35:50 -0700

    revert: revert main.cpp

--- A3/main.cpp
+++ A3/main.cpp
@@ -5,15 +5,15 @@
 #include <iostream>
 #include <cstdlib>
 #include "tests/MergeSortTests.h"
-// #include "tests/RadixSortTests.h"
-// #include "tests/LibraryRestructuringTests.h"
+#include "tests/RadixSortTests.h"
+#include "tests/LibraryRestructuringTests.h"
 #include "include/LExceptions.h"
 /*
  * This is the driver file which directs the project on testing different modules.
  * For each new testing function add a new case with the next available "module_choice" to be able to test it out.
  */
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     int module_choice = 0;
 
     if (argc > 1) {
@@ -23,10 +23,10 @@ int main(int argc, char *argv[]) {
         case 0: // Testing Assignment 2:
             std::cout << ">> MergeSort:\t\t\t\t\t\t";
             mergeSortTests();
-        // std::cout << ">> RadixSort:\t\t\t\t\t\t";
-        // radixSortTests();
-        // std::cout << ">> Library Restructuring System: \t";
-        // libraryRestructuringTests();
+            std::cout << ">> RadixSort:\t\t\t\t\t\t";
+            radixSortTests();
+            std::cout << ">> Library Restructuring System: \t";
+            libraryRestructuringTests();
             break;
         default:
             throw std::invalid_argument("Invalid module choice");
